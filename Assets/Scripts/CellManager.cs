817c9cda18e0322419726ca4651604e8c807052f
Steven Burgess <bsteven48@gmail.com>
2017-10-07 17:06:37 -0400

    Initial Commit

---
+++ Assets/Scripts/CellManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Class to help manage the state of each cell
+public class CellManager : MonoBehaviour {
+
+	private SpriteRenderer r;
+	public float val;
+
+	private float nextTime = 1;
+	private float interval = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<SpriteRenderer>();
+		val = Random.value;
+		r.color = (val >= 0.5f) ? Color.white : Color.black;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Time.time >= nextTime){
+			int numNeighbors = GetNumNeighbors();
+			if(r.color == Color.white){
+				if(numNeighbors > 3 || numNeighbors < 2){ r.color = Color.black; }
+			}else{
+				if(numNeighbors == 3){ r.color = Color.white; }
+			}
+
+			nextTime += interval;
+
+		}
+	}
+
+
+	//Gives the number of neighbors used by each cell.  
+	private int GetNumNeighbors(){
+
+		int alive = 0;
+
+		Vector3 up = 0.5f * transform.up;
+		Vector3 down = 0.5f * -transform.up;
+		Vector3 left = 0.5f * -transform.right;
+		Vector3 right = 0.5f * transform.right;
+		Vector3 upLeft = up + left;
+		Vector3 upRight = up + right;
+		Vector3 downLeft = down + left;
+		Vector3 downRight = down + right;
+
+		Vector3[] positions = {upLeft, up, upRight, left, right, downLeft, down, downRight};
+
+		RaycastHit hit;
+		foreach(Vector3 pos in positions){
+			if(Physics.Raycast(transform.position, pos, out hit)){
+				GameObject neighborCell = hit.collider.gameObject;
+				SpriteRenderer neighborRender = neighborCell.GetComponent<SpriteRenderer>();
+				if(neighborRender.color == Color.white){ alive++; }
+			}
+		}
+		return alive;
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	//the cell will become alive or dead based on the cells around it.
+	public GameObject cell;
+
+	//Even though the scale of the UI Object is 3.75, that
+	//is 50% of a unit of length in Unity
+	private const float SIZE = 0.5f;
+	private const float X_LIMIT=8.6f;
+	private const float Y_LIMIT=4.7f;
+	
+
+	// Use this for initialization
+	void Start () {
+		for(float x = -X_LIMIT; x <= X_LIMIT; x += 0.5f){
+			for(float y = -Y_LIMIT; y <= Y_LIMIT; y += 0.5f){
+				Instantiate(cell, new Vector2(x,y), Quaternion.identity);
+			}
+		}
+	}
+}
